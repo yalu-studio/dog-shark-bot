1316ffad7a534d38655b232d91b7d8aead267d82
maoyalu <345016664@qq.com>
2021-11-14 17:44:47 +1100

    Removed group check

--- dogshark/plugins/qq/notice.py
+++ dogshark/plugins/qq/notice.py
@@ -7,8 +7,7 @@ notice = on_notice()
 
 @notice.handle()
 async def _(bot: Bot, event: GroupIncreaseNoticeEvent):
-    if str(event.group_id) in bot.config.supergroups:
-        _at = f"[CQ:at,qq={event.get_user_id()}]"
-        _meme = f"[CQ:image,file={WELCOME_MEME}]"
-        msg = Message(_at + '欢迎!\n＼＼\٩(๑`^´๑)۶//／／' + _meme)
-        await notice.finish(message=msg)
+    _at = f"[CQ:at,qq={event.get_user_id()}]"
+    _meme = f"[CQ:image,file={WELCOME_MEME}]"
+    msg = Message(_at + '欢迎!\n＼＼\٩(๑`^´๑)۶//／／' + _meme)
+    await notice.finish(message=msg)
